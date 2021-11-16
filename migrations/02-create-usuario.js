8a59862f5fb1d4733adba81b58d6ff7f8645b95d
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-11-16 10:00:07 +0100

    añadimos a la migrations el campo rol

--- migrations/02-create-usuario.js
+++ migrations/02-create-usuario.js
@@ -33,6 +33,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      rol: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
