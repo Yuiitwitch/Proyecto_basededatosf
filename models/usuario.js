5e55c67c0c2ec2ee8fe53852693fe79cf65dae15
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-11-22 17:19:15 +0100

    Añadimos rol en los models para que pueda identificar con que usuario se logea

--- models/usuario.js
+++ models/usuario.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     }, 
+    rol: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
   }, {
     sequelize,
     modelName: 'usuario',
